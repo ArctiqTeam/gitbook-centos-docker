cb6c13974f37a90b203b9887ad842e97ae75f088
Matt Peterson <matt.peterson@centurylink.com>
2018-03-20 12:46:38 -0500

    fewaefzwegsx

--- bin/run.sh
+++ bin/run.sh
@@ -2,6 +2,7 @@
 if ! whoami &> /dev/null; then
   if [ -w /etc/passwd ]; then
     echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/sbin/nologin" >> /etc/passwd
+    echo "foo"
   fi
 fi
 exec "$@"
